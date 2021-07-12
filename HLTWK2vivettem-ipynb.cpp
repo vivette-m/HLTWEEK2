eb2a8f2e0d0fe314e6124884dbb874c1ed0c4e74
vivette-m <86829922+vivette-m@users.noreply.github.com>
2021-07-12 21:58:31 +0100

    Add files via upload

---
+++ HLTWK2vivettem-ipynb.cpp
@@ -0,0 +1,33 @@
+number = input ("inter a number smaller than 15")
+if number ==1 or number ==3 or number 5 or number ==7 or number ==11 or number ==13 or number==15
+print("you have entered an odd number"
+else:print("you entered an even number")
+end of chart
+
+
+
+
+
+def function-1():motorbike value yearly
+value=print ("<£2000") 
+value first year=print(">£1800)
+value second year=print(">£1,620")
+value third year=print(">£1,458")
+value forth year=print(">£1,312.2")
+value fifth year=print(">£1,181"
+value sixth yeaer= print(">1,063")
+value seventh year= print(">£957")
+output= motorbike value >("£2000") in seven years
+
+
+
+
+user-numbers = input(" enter 2 numbers between 1 and 10")
+if number(">5 and < 10")
+print("1 is subtracted ")
+elif number("> 1 and <4")
+print("1 is added")
+else number("0")
+print ("start again)
+
+
